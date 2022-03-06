2454de38d0922eba6363d01021ccc370702c1b5e
mironenko_ev@mail.ru <mironenko_ev@mail.ru>
2022-03-06 23:38:50 +0500

    Add project files.

---
+++ STL/main.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+#include<array>
+#include<vector>
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define tab "\t"
+//#define STL_ARRAY
+#define STL_VACTOR
+void main()
+{
+	setlocale(LC_ALL, "");
+#ifdef STL_ARRAY
+	std::array<int,5> arr = { 3,5,8,13,21 };
+	for (int i = 0; i < arr.size(); i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+#endif // STL_ARRAY
+#ifdef STL_VACTOR
+	std::vector<int> vec = { 0,1,1,2,3,5,8,13,21,34 };
+	for (int i = 0; i < vec.size(); i++)
+	{
+		cout << vec[i] << tab;
+	}
+	cout << endl;
+	cout << "Size: "<<vec.size() << endl;
+	cout << "MaxSize: " << vec.max_size() << endl;
+	cout << "Capacity: " << vec.capacity() << endl;
+#endif // STL_VACTOR
+
+
+}

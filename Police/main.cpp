7b54b3918882e1c1081a1b098a87f5d40558708a
mironenko_ev@mail.ru <mironenko_ev@mail.ru>
2022-03-14 21:21:55 +0500

    11

--- Police/main.cpp
+++ Police/main.cpp
@@ -11,14 +11,17 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+void save(const std::map < std::string, std::list<Crime>>base, const std::string& filename);
 void print(const std::map < std::string, std::list<Crime>>base);
+void add(std::map<std::string, std::list<Crime>>& base);
+
 void main()
 {
 	//setlocale(LC_ALL, "");
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	ofstream fout("file.txt");
+	//ofstream fout("file.txt");
 
 	std::map<std::string, std::list<Crime>> base =
 	{
@@ -28,40 +31,23 @@ void main()
 		{ "a002а" ,{Crime(2,"перекресток Ленина"),Crime(5,"улица Октябрьская") } }
 	};
 	print(base);
-	std::string licence_plate;
-	int id;
-	std::string place;
-	cout << "Введите номер автомобиля: "; cin >> licence_plate;
-	cout << "Введите правонарушение(номер статьи): "; cin >> id;
-	cout << "Введите место происшествия: "; 
-	cin.clear();
-	cin.ignore();
-	std::getline(cin, place);
-	//base[licence_plate].push_back(Crime(id, place));
+	save(base, "base.txt");
+	//add(base);
+	//print(base);
 
-	std::map<std::string, std::list<Crime>>::iterator it = base.find(licence_plate);
-	if (it != base.end())
-	{
-		it->second.push_back(Crime(id, place));
-	}
-	else
-	{
-		base.insert(std::pair<std::string, std::list<Crime>>(licence_plate, { Crime(id, place) }));
-	}
-	print(base);
-
+#ifdef DEBUG
 
 	//найти в базе автомобиль
 
 	cout << "Введите номер автомобиля: "; cin >> licence_plate;
 	//std::map<std::string, std::list<Crime>>::iterator it = base.find(licence_plate);
-	
+
 
 	it = base.find(licence_plate);
 	if (it != base.end())
 	{
 		cout << "Номер машины: " << it->first << "  совершил правонарушения: ";
-	
+
 		for (std::list<Crime>::const_iterator jt = it->second.begin(); jt != it->second.end(); ++jt)
 		{
 
@@ -71,14 +57,14 @@ void main()
 	}
 	else
 		cout << "Номер машины: " << licence_plate << "не найден!" << endl;
-	
+
 	//найти авто в диапаоне(not done)
 	std::string licence_plate1;
 	std::string licence_plate2;
-	cout << "Введите дапазон номеров от: "; cin >> licence_plate; cout <<" до "<< endl; cin >> licence_plate2;
-	it=base.lower_bound(licence_plate);
+	cout << "Введите дапазон номеров от: "; cin >> licence_plate; cout << " до " << endl; cin >> licence_plate2;
+	it = base.lower_bound(licence_plate);
 	it = base.upper_bound(licence_plate2);
-	
+
 	if (it != base.end())
 	{
 		for (std::map<string, std::list<Crime>>::const_iterator itlow = base.begin(); itup != base.end(); ++it)
@@ -98,40 +84,81 @@ void main()
 	else
 		cout << "Машин не найдено!" << endl;
 
-	
+
 	//запись в файл
-		for (std::map<string, std::list<Crime>>::const_iterator it = base.begin(); it != base.end(); ++it)
+	for (std::map<string, std::list<Crime>>::const_iterator it = base.begin(); it != base.end(); ++it)
+	{
+
+		//cout << std::left;
+		fout << it->first + ":\n";
+
+		for (std::list<Crime>::const_iterator jt = it->second.begin(); jt != it->second.end(); ++jt)
 		{
 
-			//cout << std::left;
-			fout << it->first + ":\n";
-
-			for (std::list<Crime>::const_iterator jt = it->second.begin(); jt != it->second.end(); ++jt)
-			{
-
-				fout << "\t" << *jt << ";\n";
-			}
-			fout << "\n------------------------------------\n";
+			fout << "\t" << *jt << ";\n";
 		}
-	
-	
+		fout << "\n------------------------------------\n";
+	}
+
+
 	fout.close();
 	system("notepad file.txt");
-	
+
+#endif // DEBUG
+
 }
-	void print(const std::map < std::string, std::list<Crime>>base)
+void print(const std::map < std::string, std::list<Crime>>base)
+{
+	for (std::map<string, std::list<Crime>>::const_iterator it = base.begin(); it != base.end(); ++it)
 	{
-		for (std::map<string, std::list<Crime>>::const_iterator it = base.begin(); it != base.end(); ++it)
+
+		//cout << std::left;
+		cout << it->first + ":\n";
+
+		for (std::list<Crime>::const_iterator jt = it->second.begin(); jt != it->second.end(); ++jt)
 		{
 
-			//cout << std::left;
-			cout << it->first + ":\n";
-
-			for (std::list<Crime>::const_iterator jt = it->second.begin(); jt != it->second.end(); ++jt)
-			{
-
-				cout << "\t" << *jt << ";\n";
-			}
-			cout << "\n------------------------------------\n";
+			cout << "\t" << *jt << ";\n";
 		}
+		cout << "\n------------------------------------\n";
 	}
+}
+void add(std::map<std::string, std::list<Crime>>& base)
+{
+	std::string licence_plate;
+	int id;
+	std::string place;
+	cout << "Введите номер автомобиля: "; cin >> licence_plate;
+	cout << "Введите правонарушение(номер статьи): "; cin >> id;
+	cout << "Введите место происшествия: ";
+	cin.clear();
+	cin.ignore();
+	std::getline(std::cin, place);
+	//base[licence_plate].push_back(Crime(id, place));
+
+	std::map<std::string, std::list<Crime>>::iterator it = base.find(licence_plate);
+	if (it != base.end())
+	{
+		it->second.push_back(Crime(id, place));
+	}
+	else
+	{
+		base.insert(std::pair<std::string, std::list<Crime>>(licence_plate, { Crime(id, place) }));
+	}
+}
+void save(const std::map < std::string, std::list<Crime>>base, const std::string& filename)
+{
+	std::ofstream fout(filename);
+	for (std::pair<std::string, std::list<Crime>>i : base)
+	{
+		fout << i.first << ":\t";
+		for (Crime j : i.second)
+		{
+			fout << j << ", ";
+		}
+		fout << ";\n";
+	}
+	fout.close();
+	std::string command = std::string("start notepad ") + filename;
+	system(command.c_str());
+}
